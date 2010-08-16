12fbb5a55722bf64590dab7e0271d0e1b3bb1d7b
koryk <kory.kirk@gmail.com>
2010-08-16 14:20:51 -0400

    added more data specefic to rend points

--- src/org/torproject/jtor/hiddenservice/RendezvousPoint.java
+++ src/org/torproject/jtor/hiddenservice/RendezvousPoint.java
@@ -1,5 +1,6 @@
 package org.torproject.jtor.hiddenservice;
 
+import org.torproject.jtor.crypto.TorPrivateKey;
 import org.torproject.jtor.crypto.TorPublicKey;
 import org.torproject.jtor.data.HexDigest;
 import org.torproject.jtor.data.IPv4Address;
@@ -13,7 +14,8 @@ import org.torproject.jtor.directory.impl.RouterImpl;
  * The Class RendezvousPoint.
  */
 public class RendezvousPoint extends RouterImpl{
-		
+	TorPrivateKey keys;	
+	byte[] rendCookie = new byte[20];
 	/**
 	 * Instantiates a new rendezvous point.
 	 * 
@@ -22,5 +24,20 @@ public class RendezvousPoint extends RouterImpl{
 	 */
 	public RendezvousPoint(RouterStatus status){
 		super(status);
-	}			
+	}
+	public byte[] getRendezvousCookie(){
+		return rendCookie;
+	}
+	public void setRendezvousCookie(byte[] cookie){
+		rendCookie = cookie;
+	}
+	public void setOnionKey(TorPrivateKey keys){
+		this.keys = keys;
+	}
+	public TorPublicKey getRendKey(){
+		if (keys == null)
+			return null;
+		else
+			return keys.getPublicKey();
+	}
 }
